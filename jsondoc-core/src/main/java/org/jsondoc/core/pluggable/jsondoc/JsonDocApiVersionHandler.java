aad352baf4aeb541a00915655c4c2e578fd74494
Daniel Ostermeier <dostermeier@ebuddy.com>
2013-11-09 23:49:40 +0100

    TECH: fix failing test.

--- jsondoc-core/src/main/java/org/jsondoc/core/pluggable/jsondoc/JsonDocApiVersionHandler.java
+++ jsondoc-core/src/main/java/org/jsondoc/core/pluggable/jsondoc/JsonDocApiVersionHandler.java
@@ -2,6 +2,8 @@ package org.jsondoc.core.pluggable.jsondoc;
 
 import java.lang.annotation.Annotation;
 import java.lang.reflect.AnnotatedElement;
+import java.lang.reflect.Field;
+import java.lang.reflect.Member;
 
 import org.jsondoc.core.annotation.ApiVersion;
 import org.jsondoc.core.pluggable.ApiAnnotationHandler;
@@ -10,6 +12,7 @@ import org.jsondoc.core.pluggable.ApiObjectAnnotationHandler;
 import org.jsondoc.core.pojo.ApiDoc;
 import org.jsondoc.core.pojo.ApiMethodDoc;
 import org.jsondoc.core.pojo.ApiObjectDoc;
+import org.jsondoc.core.pojo.ApiObjectPropertyDoc;
 import org.jsondoc.core.pojo.ApiVersionDoc;
 
 /**
@@ -27,18 +30,35 @@ public class JsonDocApiVersionHandler implements ApiAnnotationHandler,
     @Override
     public void handle(AnnotatedElement element, ApiObjectDoc doc) {
         ApiVersion annotation = element.getAnnotation(ApiVersion.class);
-        doc.setVersion(new ApiVersionDoc(annotation.since(), annotation.until()));
+        if (element instanceof Class) {
+            doc.setVersion(new ApiVersionDoc(annotation.since(), annotation.until()));
+        }
+        if (element instanceof Member) {
+            Member member = (Member)element;
+            ApiObjectPropertyDoc property = doc.getField(member.getName());
+            if (property == null) {
+                property = new ApiObjectPropertyDoc();
+                property.setActualName(member.getName());
+                property.setName(member.getName());
+                doc.getFields().add(property);
+            }
+            property.setVersion(new ApiVersionDoc(annotation.since(), annotation.until()));
+        }
     }
 
     @Override
     public void handle(AnnotatedElement element, ApiMethodDoc doc) {
         ApiVersion annotation = element.getAnnotation(ApiVersion.class);
-        doc.setVersion(new ApiVersionDoc(annotation.since(), annotation.until()));
+        if (doc.getVersion() == null) {
+            doc.setVersion(new ApiVersionDoc(annotation.since(), annotation.until()));
+        }
     }
 
     @Override
     public void handle(AnnotatedElement element, ApiDoc doc) {
         ApiVersion annotation = element.getAnnotation(ApiVersion.class);
-        doc.setVersion(new ApiVersionDoc(annotation.since(), annotation.until()));
+        if (doc.getVersion() == null) {
+            doc.setVersion(new ApiVersionDoc(annotation.since(), annotation.until()));
+        }
     }
 }
