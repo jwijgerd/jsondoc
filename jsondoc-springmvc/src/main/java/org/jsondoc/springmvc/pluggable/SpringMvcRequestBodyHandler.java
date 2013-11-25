b621bc0155254d651aee12e0bdd9c6a19260a9e3
geert <gvos@ebuddy.com>
2013-11-25 16:22:06 +0100

    Working support for Request Body parameter.

--- jsondoc-springmvc/src/main/java/org/jsondoc/springmvc/pluggable/SpringMvcRequestBodyHandler.java
+++ jsondoc-springmvc/src/main/java/org/jsondoc/springmvc/pluggable/SpringMvcRequestBodyHandler.java
@@ -1,41 +1,56 @@
 package org.jsondoc.springmvc.pluggable;
 
-import static org.jsondoc.core.util.JSONDocSupport.getBodyObject;
-import static org.jsondoc.core.util.JSONDocSupport.isMultiple;
+import static org.jsondoc.core.util.JSONDocSupport.*;
 
 import java.lang.annotation.Annotation;
 import java.lang.reflect.AnnotatedElement;
+import java.lang.reflect.Method;
 
 import org.jsondoc.core.pluggable.ApiMethodAnnotationHandler;
 import org.jsondoc.core.pojo.ApiBodyObjectDoc;
 import org.jsondoc.core.pojo.ApiMethodDoc;
-import org.jsondoc.core.util.Parameter;
 import org.springframework.web.bind.annotation.RequestBody;
 
 /**
  * @author Daniel Ostermeier
+ * @author Geert Vos
  */
 public class SpringMvcRequestBodyHandler implements ApiMethodAnnotationHandler {
 
     @Override
     public boolean canHandle(AnnotatedElement candidate, Annotation annotation) {
-        return annotation instanceof RequestBody && candidate instanceof Parameter;
+    	return candidate instanceof Method && findRequestBodyType((Method)candidate) >= 0;
     }
 
-    @Override
-    public void handle(AnnotatedElement element, ApiMethodDoc doc) {
 
-        Parameter parameter = (Parameter)element;
+	@Override
+	public void handle(AnnotatedElement element, ApiMethodDoc doc) {
+		Method m = (Method)element;
+		int index = findRequestBodyType(m);
 
+        String[] objectDetails = getBodyObject(m, index);
         ApiBodyObjectDoc body = new ApiBodyObjectDoc();
-        String[] objectDetails = getBodyObject(parameter.getMethod(), parameter.getIndex());
         body.setObject(objectDetails[0]);
         body.setMapKeyObject(objectDetails[1]);
         body.setMapValueObject(objectDetails[2]);
-        body.setMultiple(String.valueOf(isMultiple(parameter.getParameterType())));
         body.setMap(objectDetails[3]);
-        doc.setBodyobject(body);
-    }
+        body.setMultiple("false");
+		doc.setBodyobject(body);
+	}
+	
+	private int findRequestBodyType(Method m) {
+		Annotation[][] annotations = m.getParameterAnnotations();
+		for(int i=0;i<annotations.length;i++) {
+			for(Annotation a : annotations[i]) {
+				if(a instanceof RequestBody) {
+					return i;
+				}
+			}
+		}
+		return -1;
+	}
+
+    
 }
 
 
